862488794cfb48d570a627ad093af90ccce7f72a
Abeer Shahid <abeer.shahid1@gmail.com>
2024-06-06 22:19:00 +0100

    adjusted hero section

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -69,18 +69,17 @@ export type SectionId = (typeof SectionId)[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `I'm Abeer Shahid.`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        I'm a Northern Ireland based<strong className="text-stone-100"> Automation Engineer</strong>, currently working
+        at <strong className="text-stone-100">WuXi Biologics</strong> helping build a first in class biopharma manufacturing site.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        In my free time time, you can catch me watching <strong className="text-stone-100">Man Utd</strong>,
+        playing with <strong className="text-stone-100">my son</strong>, or writing/podcasting for my blog{' '}
+        <strong className="text-stone-100">Manara</strong>.
       </p>
     </>
   ),
